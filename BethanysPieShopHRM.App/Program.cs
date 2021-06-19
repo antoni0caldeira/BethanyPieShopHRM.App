9a96162d659a8b090adfc7a20fd84fc94b350603
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-19 12:35:44 +0100

    Updated with the course code

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,14 +1,14 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BethanyPieShopHRM.App
+namespace BethanysPieShopHRM.App
 {
     public class Program
     {
@@ -17,7 +17,7 @@ namespace BethanyPieShopHRM.App
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
